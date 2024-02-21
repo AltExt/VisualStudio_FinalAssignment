967547ca2ca5a3ee80013c14d0e83a0b287306c6
AltExt <52500808+AltExt@users.noreply.github.com>
2024-02-21 16:30:05 +0000

    Assignment requirements added

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function NewColor() {
+	document.getElementById("welcomeH1").style.color = "red";
+}
+
+function OldColor() {
+	document.getElementById("welcomeH1").style.color = "blue";
+}
